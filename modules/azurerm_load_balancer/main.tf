2d46588a6344b0a516806076d624fd86a18913d4
rishu-raj007 <rahul747979@gmail.com>
2025-09-16 20:29:37 +0530

    health probe added

--- modules/azurerm_load_balancer/main.tf
+++ modules/azurerm_load_balancer/main.tf
@@ -7,11 +7,16 @@ resource "azurerm_lb" "lb" {
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
+    public_ip_address_id = azurerm_public_ip.lb.id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "lb_pool" {
   loadbalancer_id = azurerm_lb.lb.id
   name            = "BackEndAddressPool"
+}
+resource "azurerm_lb_probe" "example" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "ssh-running-probe"
+  port            = 80
 }
